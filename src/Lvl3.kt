cad9c73d20c244401dd4649f9de9ebef910d4496
Max <kozlyakov.m@gmail.com>
2020-02-28 20:21:57 +0700

    set instead list

--- src/Lvl3.kt
+++ src/Lvl3.kt
@@ -1,11 +1,12 @@
 
 fun main (args:Array<String>){
-    var result:MutableList<String> = mutableListOf()
+    val result:MutableSet<String> = mutableSetOf()
     for(str in args){
         if(!result.contains(str))
-            result.add(str);
+            result.add(str)
+        
     }
-    result.sort()
+    result.toMutableList().sort()
 
     for(str in result){
         println(str)
