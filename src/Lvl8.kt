0693d2812bfa440a996bee76010a460ee298c43a
Max <kozlyakov.m@gmail.com>
2020-02-28 21:22:08 +0700

    decomposition

--- src/Lvl8.kt
+++ src/Lvl8.kt
@@ -4,53 +4,63 @@ import kotlin.system.exitProcess
 
 fun main(args:Array<String>){
 
-    val message:Array<String>
+    val text:Array<String> = getText(args)
 
-    if(args.isEmpty()){
-        message = readLine()!!.split(" ").toTypedArray()
-    }
-    else{
-        if(args[0]=="-f"){
-           try{
-               if(args.size > 1){
-                   message = File(args[1]).readText().split(" ").toTypedArray()
-               }
-               else{
-                   println("Не указано имя файла")
-                   exitProcess(1)
-               }
-           }
-           catch (e:FileNotFoundException){
-                println(e.message)
-                exitProcess(1)
-           }
-        }
-        else{
-            message = args
-        }
-    }
-
-    val words:LinkedHashMap<String, Int> = linkedMapOf()
-    for(str in message){
-
-        if(words.containsKey(str)){
-            val freq = words[str]
-            words.put(str, freq!!.plus(1) ) // почему-то теперь не дает сделать просто freq+1
-        }
-        else{
-            words.put(str, 1);
-        }
-    }
+    val words:LinkedHashMap<String, Int> = findUnique(text)
 
     val comparator = compareByDescending<Pair<String, Int>>{it.second}.thenBy {it.first}
     val result:List<Pair<String, Int>> = words.toList().sortedWith(comparator)
 
+    output(result)
 
+}
+
+fun getText(args: Array<String>):Array<String>{
+
+    if(args.isEmpty()){
+        return readLine()!!.split(" ").toTypedArray()
+    }
+    else{
+        if(args[0]=="-f"){
+            try{
+                if(args.size > 1){
+                    return File(args[1]).readText().split(" ").toTypedArray()
+                }
+                else{
+                    println("Не указано имя файла")
+                    exitProcess(1)
+                }
+            }
+            catch (e:FileNotFoundException){
+                println(e.message)
+                exitProcess(1)
+            }
+        }
+        else{
+            return args
+        }
+    }
+}
+
+fun findUnique(text:Array<String>):LinkedHashMap<String, Int>{
+    val result:LinkedHashMap<String, Int> = linkedMapOf()
+    for(str in text){
+
+        if(result.containsKey(str)){
+            val freq = result[str]
+            result.put(str, freq!!.plus(1) ) // почему-то теперь не дает сделать просто freq+1
+        }
+        else{
+            result.put(str, 1);
+        }
+    }
+    return result
+}
+
+fun output(result:List<Pair<String, Int>>){
     for((word, freq) in result){
         println("""$word $freq""")
-
     }
-
 }
 
 
