ae1b787d5e7e512a69b51059f11977500b4f3ddc
Max <kozlyakov.m@gmail.com>
2020-03-02 22:20:02 +0700

    fixed #3

--- src/Lvl4.kt
+++ src/Lvl4.kt
@@ -1,15 +1,7 @@
 fun main(args: Array<String>) {
-
-    val result: LinkedHashMap<String, Int> = linkedMapOf()
-    for (str in args) {
-        val freq = result[str] ?: 0
-        result.put(str, freq+1)
-    }
-
-    for ((word, freq) in result) {
-        println("""$word $freq""")
-    }
-
+    args.sort()
+    val result = args.groupingBy { it }.eachCount()
+    result.forEach { (word, freq) -> println("$word : $freq") }
 }
 
 
